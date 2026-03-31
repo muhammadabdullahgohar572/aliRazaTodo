9736c059792bba03a4636bc6d15adbf14a40f1cf
Abdullah <muhammadabdullahgohar572@gmail.com>
2026-03-31 22:36:52 +0500

    first commit

--- src/app/helper/HttpsHelper.js
+++ src/app/helper/HttpsHelper.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export const httAxios = axios.create({
   baseURL:
     process.env.NODE_ENV === "production"
-      ? "https://tdodapp-msadt57dx-muhammad-abdullahs-projects-036c9e90.vercel.app"
+      ? "https://ali-raza-todo.vercel.app"
       : "http://localhost:3000", // Include http:// for localhost
 });
