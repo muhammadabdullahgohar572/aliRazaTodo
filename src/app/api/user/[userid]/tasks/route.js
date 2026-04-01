9d955613c9153094f80e1f121d0228f3a4169a41
Abdullah <muhammadabdullahgohar572@gmail.com>
2026-04-01 14:11:40 +0500

    first commit

--- src/app/api/user/[userid]/tasks/route.js
+++ src/app/api/user/[userid]/tasks/route.js
@@ -2,19 +2,19 @@ import { Task } from "@/app/model/Task";
 import { NextResponse } from "next/server";
 
 export const GET = async (request, { params }) => {
-    
-    try {
-
-    const { userid } = params;   
+  try {
+    const { userid } =await params;
     const tasks = await Task.find({ userId: userid });
     return NextResponse.json(tasks);
-  
-
-} catch (error) {
+  } catch (error) {
     console.error("Error retrieving tasks:", error);
     return NextResponse.json(
-      { message: "Failed to retrieve tasks", status: false, error: error.message },
-      { status: 500 }
+      {
+        message: "Failed to retrieve tasks",
+        status: false,
+        error: error.message,
+      },
+      { status: 500 },
     );
   }
 };
